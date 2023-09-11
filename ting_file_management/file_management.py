0003c7f51364a9c920ef0a98c16a21b5c4c851a6
jimmyarbats <jimmyaraujob@gmail.com>
2023-09-11 12:11:57 -0300

    req 2 done: implements txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            sys.stderr.write("Formato inválido\n")
+            return None
+        with open(path_file, "r") as file:
+            return [line.strip() for line in file]
+    except FileNotFoundError as e:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return None
