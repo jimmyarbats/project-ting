76caf28bf63e7a71eba1216b41e1afc0fce459cf
jimmyarbats <jimmyaraujob@gmail.com>
2023-09-11 12:34:13 -0300

    req 7 done: implements exists_word func

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,26 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for file_index in range(len(instance)):
+        file_data = instance.search(file_index)
+        events = [
+            {"linha": line_num}
+            for line_num, line in enumerate(
+                file_data["linhas_do_arquivo"], start=1
+            )
+            if word.lower() in line.lower()
+        ]
+
+        if events:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_data["nome_do_arquivo"],
+                    "ocorrencias": events,
+                }
+            )
+
+    return result
 
 
 def search_by_word(word, instance):
